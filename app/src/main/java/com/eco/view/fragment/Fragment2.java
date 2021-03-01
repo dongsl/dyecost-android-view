0c9ed327ac4f345490d95a36b9b158d141c4b223
董士亮(Connor) <77666591@qq.com>
2021-03-01 10:07:33 +0800

    [CODE]:20210301 1).增加demo

--- app/src/main/java/com/eco/view/fragment/Fragment2.java
+++ app/src/main/java/com/eco/view/fragment/Fragment2.java
@@ -8,11 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-
 import com.eco.basics.handler.DensityHandler;
 import com.eco.basics.handler.Num;
 import com.eco.view.DySlideButtonView;
